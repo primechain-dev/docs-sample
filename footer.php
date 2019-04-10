724c9b18abe22ddace49cf0e4c837df6eda98486
primechain-dev <sripathi@primechain.in>
2019-04-10 22:18:54 +0530

    First commit

---
+++ footer.php
@@ -0,0 +1,29 @@
+
+			</div><!--//container-->
+		</div><!--//doc-wrapper-->
+		
+		<div id="promo-block" class="promo-block">
+			<div class="container">
+			</div><!--//container-->
+		</div><!--//promo-block-->
+		
+	</div><!--//page-wrapper-->
+	
+	<footer id="footer" class="footer text-center">
+		<div class="container">
+			<small class="copyright">Designed and Maintained by Primechain Technologies Pvt. Ltd.</small>
+		</div><!--//container-->
+	</footer><!--//footer-->
+	
+	 
+	<!-- Main Javascript -->          
+	<script type="text/javascript" src="assets/plugins/jquery-3.3.1.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/plugins/prism/prism.js"></script>    
+	<script type="text/javascript" src="assets/plugins/jquery-scrollTo/jquery.scrollTo.min.js"></script>      
+	<script type="text/javascript" src="assets/plugins/stickyfill/dist/stickyfill.min.js"></script>                                                             
+	<script type="text/javascript" src="assets/js/main.js"></script>
+	
+</body>
+</html> 
+
---
+++ nav.php
@@ -0,0 +1,21 @@
+
+                    <div class="doc-sidebar col-md-3 col-12 order-0 d-none d-md-flex">
+                        <div id="doc-nav" class="doc-nav">
+	                        
+	                            <nav id="doc-menu" class="nav doc-menu flex-column sticky">
+	                                <a class="nav-link scrollto" href="#primechain-api">Primechain API</a>
+	                                <a class="nav-link scrollto" href="#introduction">1. Introduction</a>
+                                    <nav class="doc-sub-menu nav flex-column">
+                                        <a class="nav-link scrollto" href="#1-1-blockchain-benefits">1.1 Blockchain benefits</a>
+                                        <a class="nav-link scrollto" href="#1-2-blockchain-pain-points">1.2 Blockchain pain points</a>
+                                        <a class="nav-link scrollto" href="#1-3-primechain-api">1.3 Primechain API</a>
+                                    </nav><!--//nav-->
+	                                <a class="nav-link scrollto" href="#2-primechain-api-use-cases">2. Primechain API use cases</a>
+                                    <a class="nav-link scrollto" href="#3-documentation">3. Documentation</a>
+	                                <a class="nav-link scrollto" href="#4-sandbox">4. Sandbox</a>
+	                                <a class="nav-link scrollto" href="#5-about-primechain-technologies">5. About Primechain Technologies</a>
+                                    <a class="nav-link scrollto" href="#6-sample-codebase">6. Sample Codebase</a>
+	                            </nav><!--//doc-menu-->
+	                        
+                        </div>
+                    </div><!--//doc-sidebar-->
